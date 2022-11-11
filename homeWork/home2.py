e237c4452189e6d7782db7e1c3b8b81d569b262e
tbokirov <tbokirov@gmail.com>
2022-11-11 17:31:03 +0600

    lol5

--- homeWork/home2.py
+++ homeWork/home2.py
@@ -35,54 +35,37 @@ player3.greetings()
 player4 = Hero(name = 'Palash', nickname = 'Bocman', heatpoint = 5 , damage = 10)
 print(player4.name , player4.nickname , player4.hp , player4.damage)
 player4.brand_phrase()
-
-
 class Air(Hero):
     wings = 2
-
     def __init__(self, name, nickname, hp, damage, fly=False):
         super().__init__(name, nickname, hp, damage)
         self.fly = fly
-
     def brand_phrase(self):
         self.fly = True
         print('fly in the True_phrase')
-
-    def __Gen_x(self):
+    def _gutenmorgen(self):
         pass
-
-
 class Space(Hero):
-    tentacles = 12
-
+    hands = 12
     def __init__(self, name, nickname, hp, damage, fly=False):
         super().__init__(name, nickname, hp, damage)
         self.fly = fly
-
     def brand_phrase(self):
         self.fly = True
         print('fly in the True_phrase')
-
-    def __Gen_x(self):
+    def __gutentak(self):
         pass
-
-
 class Earthly(Hero):
-    legs = 2
-
+    ankles = 2
     def __init__(self, name, nickname, hp, damage, fly=False):
         super().__init__(name, nickname, hp, damage)
         self.fly = fly
-
     def brand_phrase(self):
         self.fly = True
         print('fly in the True_phrase')
-
-    def __Gen_x(self):
+    def __gutenabend(self):
         pass
-
-
-h1 = Air(name='Karl', nickname='Invoker', hp=780, damage=60)
-h2 = Space(name='Pudge', nickname='Butcher', hp=800, damage=90)
-h3 = Earthly(name='Gustavo', nickname='Gus', hp=1000, damage=200)
+h1 = Air(name='soap', nickname='maktvish', hp=780, damage=60)
+h2 = Space(name='ghost', nickname='...', hp=800, damage=90)
+h3 = Earthly(name='gas', nickname='goose', hp=1000, damage=200)
 h1.brand_phrase()
